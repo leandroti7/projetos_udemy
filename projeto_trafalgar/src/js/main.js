752452d651fbfd559de105ad9f91eb579acfa105
leandro souza <leandro.dasilvati7@gmail.com>
2020-08-18 23:48:08 -0300

    finalizando ambiente produção webpack

---
+++ projeto_trafalgar/src/js/main.js
@@ -0,0 +1,7 @@
+import '../sass/style.sass';
+import '../assets/BG_OK_MOBILE.png';
+
+$(document).ready(function () {
+    alert('Ta indo');
+});
+console.log('foi');
---
+++ projeto_trafalgar/webpack.config.js
@@ -0,0 +1,70 @@
+//Configuração webpack
+
+//incluindo plugins com metodo es6
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    //entry: entrada arquivo no qual vai estar o script js
+    //output: saida com nome e destino no qual vai ser criado o script minificado
+    entry: './src/js/main.js',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    //instanciando plugins usados para minificar ou manipular o html e o css
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './public/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'app.css'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new webpack.SourceMapDevToolPlugin({})
+    ],
+    //Modulos com regras para conversão de arquivos de estulização especificos para css
+    module: {
+        rules: [
+            {   //regex com quais tipo de arquivos a ser convertido
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {   //regex com regra para converter css in line do js
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {   //regex com regra para converter arquivo js(es6) em js(es5)
+                //para navegadores antigos com utilização do plugin babel
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(jge?g|png|gif|svg)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]'
+                }
+            }
+        ]
+    }
+}
